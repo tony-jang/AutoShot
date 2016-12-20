2b8b2a98c5137e0fd31d599702a72d130370447b
PlurdisDesk <uutak2000>
2016-12-20 23:37:19 +0900

    Add NotificationSounds Class

---
+++ AutoCapturer/Sounds/NotificationSounds.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoCapturer.Sounds
+{
+    static class NotificationSounds
+    {
+        public static void PlayNotificationSound(SoundType soundtype)
+        {
+            if (soundtype == SoundType.AuCaModeOn)
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer($"snd2.wav");
+                player.Play();
+            }
+            else if (soundtype == SoundType.AuCaModeOff)
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer($"snd1.wav");
+                player.Play();
+            }
+        }
+        
+        public enum SoundType
+        {
+            AuCaModeOn, AuCaModeOff
+        }
+    }
+}
